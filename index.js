8d882d5bed5f9d7d6286224ae9d3846194ed67de
TabrezM <116564258+TabrezMZ@users.noreply.github.com>
2022-12-13 04:19:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var readlineSync = require('readline-sync');
+var score = 0;
+
+
+function welcome() {
+  var userName = readlineSync.question("Hey , What's your name? ");
+
+  console.log("Welcome " + userName + " to DO YOU KNOW Mansuri?");
+}
+
+welcome();
+
+var questions = [
+  {
+    question: "What is my Passion ?",
+    answer: "coding"
+  },
+  {
+    question: "what is my favourite language in technology  ?",
+    answer: "javascript"
+  },
+  {
+    question: "where do i live ?",
+    answer: "india"
+  }
+]
+
+
+function playGame() {
+  for (let i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    var userAnswer = readlineSync.question(currentQuestion.question);
+    if (currentQuestion.answer == userAnswer) {
+      console.log("you are right");
+      score = score + 1;
+    } else {
+      console.log("You are wrong");
+    }
+  }
+}
+
+playGame();
+
+console.log("Ya'y Finally your Score is :" + score);
