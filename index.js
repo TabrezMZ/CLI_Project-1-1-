f81b15ea5388d1a8e922f97a02e98607ad75cb14
TabrezM <116564258+TabrezMZ@users.noreply.github.com>
2022-12-31 14:10:22 +0000

    make MCQ quiz

--- index.js
+++ index.js
@@ -7,39 +7,67 @@ function welcome() {
   var userName = readlineSync.question("Hey , What's your name? ");
 
   console.log("Welcome " + userName + " to DO YOU KNOW Mansuri?");
-} 
+
+  const pressKey = readlineSync.question('Hit Enter key to start.', { hideEchoBack: true, mask: '' })
+
+  console.log(pressKey)
+
+  console.log("Here goes the first question" + " (Remember - you have to press the key assigned to the write answer)")
+
+}
 
 welcome();
 
 var questions = [
   {
-    question: "What is my Passion ?",
-    answer: "coding"
+    question: "What is his favourite color? ",
+    answer: ['White', 'Blue', 'Pink', 'Black'],
+    answerOpt: 'Black'
   },
   {
-    question: "what is my favourite language in technology  ?",
-    answer: "javascript"
+    question: "Which brand laptop i used ? ",
+    answer: ['hp', 'dell', 'apple', 'asus'],
+    answerOpt: 'hp'
   },
   {
-    question: "where do i live ?",
-    answer: "india"
+    question: "where i live? ",
+    answer: ['India', 'America', 'Africa', 'London'],
+    answerOpt: 'India'
+  },
+  {
+    question: " Whick brand Mobile i used ",
+    answer: ['vivo', 'apple', 'oppo', 'redmi'],
+    answerOpt: 'oppo'
+  },
+  {
+    question: "what is my hobby? ",
+    answer: ['Coding', 'swimming', 'running', 'Reading'],
+    answerOpt: 'Coding'
+  },
+  {
+    question: "what is my age? ",
+    answer: ['23', '24', '22', '26'],
+    answerOpt: '22'
   }
 ]
 
 
-function playGame() {
-  for (var i = 0; i < questions.length; i++) {
-    var currentQuestion = questions[i];
-    var userAnswer = readlineSync.question(currentQuestion.question);
-    if (currentQuestion.answer == userAnswer) {
-      console.log("you are right");
-      score = score + 1;
-    } else {
-      console.log("You are wrong");
-    }
+questions.map((DB) => {
+  let userAnswer = readlineSync.keyInSelect(DB.answer, DB.question)
+  if (DB.answer[userAnswer] === DB.answerOpt) {
+    score = score + 1
+    console.log("Yes, you are right")
+    console.log("And your score is: " + score)
+  } else {
+    console.log("Oops, you are wrong")
+    console.log("And your score is: " + score)
   }
-}
+  console.log("--------------------------------");
+})
 
-playGame();
+console.log("finally Yor score is : " + score)
 
-console.log("Finally your Score is : " + score);
+
+
+
+
